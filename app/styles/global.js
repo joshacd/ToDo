527b6d99d93e69db3eb6f18f50aca8e814c67808
Josha Chapman-Dodson <joshacd@gmail.com>
2026-03-14 22:26:47 -0700

    Add global stylesheet

---
+++ app/styles/global.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+	flex: 1,
+	padding: 40,
+  },
+  title: {
+	fontSize: 24,
+	fontWeight: 'bold',
+	marginBottom: 16,
+  },
+  input: {
+	borderWidth: 1,
+	borderColor: '#ccc',
+	padding: 10,
+	marginBottom: 10,
+	borderRadius: 6,
+  },
+  taskRow: {
+	flexDirection: 'row',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+	borderBottomWidth: 1,
+	borderColor: '#eee',
+  },
+  task: {
+	fontSize: 18,
+	paddingVertical: 8,
+  },
+  deleteButton: {
+	backgroundColor: '#e53935',
+	paddingHorizontal: 12,
+	paddingVertical: 6,
+	borderRadius: 6,
+  },
+  deleteText: {
+	color: '#fff',
+	fontWeight: 'bold',
+  },
+});
+
+export default styles;
