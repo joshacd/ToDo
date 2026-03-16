7f647e60f652d27d1266d233a4c8cbb81acc0261
Josha Chapman-Dodson <joshacd@gmail.com>
2026-03-16 12:11:40 -0700

    Add Details, Settings screens

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack>
-    <Stack.Screen name="index" options={{ title: "To-Do List" }} />
+    <Stack.Screen name="index" options={{ title: "ToDo" }} />
+    <Stack.Screen name="details" options={{ title: "Details"}} />
+    <Stack.Screen name="settings" options={{ title: "Settings"}} />
   </Stack>
 }
