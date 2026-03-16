8a91219037c33f22894d8fac88eec56fa73b13bc
Josha Chapman-Dodson <joshacd@gmail.com>
2026-03-16 12:11:55 -0700

    Add export function

---
+++ app/details.jsx
@@ -0,0 +1,10 @@
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+import styles from './styles/global';
+
+export default function Details() {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Details</Text>
+		</View>
+	);
+}
---
+++ app/settings.jsx
@@ -0,0 +1,10 @@
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+import styles from './styles/global';
+
+export default function Settings() {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Setting</Text>
+		</View>
+	);
+}
