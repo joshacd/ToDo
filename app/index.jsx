82d6f1e113a473695f93a98781c98918a747f31e
Josha Chapman-Dodson <joshacd@gmail.com>
2026-03-16 12:12:22 -0700

    Add Settings, Details pressables

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+import { Stack, useRouter } from "expo-router";
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity, Pressable } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import styles from './styles/global';
 
 const App = () => {
+  const router = useRouter();
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
@@ -18,6 +21,18 @@ const App = () => {
   };
 
   return (
+    <>
+    <Stack.Screen
+      options={{
+        title: 'ToDo',
+        headerRight: () => (
+          <Pressable onPress={() => router.push('/settings')}>
+            <Ionicons name="settings-outline" size={24} color="#000" />
+          </Pressable>
+        ),
+      }}
+    />
+    
     <View style={styles.container}>
       <Text style={styles.title}>To Do List</Text>
       <TextInput
@@ -34,15 +49,21 @@ const App = () => {
           <View style={styles.taskRow}>
             <Text style={styles.task}>{item.text}</Text>
             <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={() => router.push('/details')}>
+              <Ionicons name="information-circle-outline" size={24} color="#000" />
+            </TouchableOpacity>
+            <TouchableOpacity
               style={styles.deleteButton}
               onPress={() => deleteTask(item.id)}
             >
-              <Text style={styles.deleteText}>Delete</Text>
+              <Ionicons name="trash-outline" size={24} color="#FFF" />
             </TouchableOpacity>
           </View>
         )}
       />
     </View>
+    </>
   );
 };
 
